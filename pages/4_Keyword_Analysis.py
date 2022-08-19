2e4acc68e712b878ac7f2ae5c4fcd306cc86d1e2
Nishan Obeyesekera <108488940+NishanD21@users.noreply.github.com>
2022-08-20 00:13:42 +0530

    Update 4_Keyword_Analysis.py

--- pages/4_Keyword_Analysis.py
+++ pages/4_Keyword_Analysis.py
@@ -1,4 +1,4 @@
-#..............................UI design of Polarity Analysis.....................................
+#..............................UI design of Keyword Analysis.....................................
 
 #Importing necessary Libraries
 
@@ -6,15 +6,18 @@ import pandas as pd # for Dataframes manipulation
 import numpy as np # for arrays manipulation
 import seaborn as sns # foe data visualization
 import plotly.express as px # for data visualization
+import matplotlib.pyplot as plt # for data visualization
 from PIL import Image # to import images to User Interface (UI)
 import requests # to assist importing images to User Interface (UI)
 from io import BytesIO # to assist importing images to User Interface (UI)
-from textblob import TextBlob #For natural language processing
+from requests_html import HTMLSession # to extract the words from YouTube channel
+from rake_nltk import Rake # to extract keywords
+from fuzzywuzzy import fuzz #for hashtag rating
 import streamlit as st # for UI design
 
-#....................Polarity Analysis of YouTube Comments..............................
+#Importing Images to user interface (UI)
 
-url0 = "https://raw.githubusercontent.com/NishanD21/SNA/main/Images/pOLARITY.png"
+url0 = "https://raw.githubusercontent.com/NishanD21/SNA/main/Images/Keyword%20Analysis.png"
 url1 = "https://raw.githubusercontent.com/NishanD21/SNA/main/Images/FB1.png"
 url2 = "https://raw.githubusercontent.com/NishanD21/SNA/d4d282cba51f29e11e4cb91c61fe4431ef88f7fd/Images/YT1.png"
 url3 = "https://raw.githubusercontent.com/NishanD21/SNA/main/Images/Insta.png"
@@ -48,97 +51,164 @@ st.sidebar.markdown("<h2 style='text-align: center;'> </h2>", unsafe_allow_html=
 
 st.sidebar.image(img00)
 
+#.................... Hashtag Analysis of YouTube............................
 
-df_yt_com = pd.read_json('https://raw.githubusercontent.com/NishanD21/SNA/main/Information%20Diffusion/YT_Comments.json')
+#text extraction from YouTube page
 
-#Polarity Analysis using NLP
+def extract_text_yt():
+    s_yt = HTMLSession()
+    url_yt = 'https://www.youtube.com/c/ALKuppiya'
+    response_yt = s_yt.get(url_yt)
+    return response_yt.html.find(first=True).text
 
-comments = df_yt_com["Comment"]
-polarity =[]
+# Separating keywords 
 
-for comment in comments:
-    review_1 = TextBlob(comment)
-    rev1 = review_1.sentiment.polarity
-    polarity.append(rev1)
+keywords_yt= []
+rating_yt =[]
 
-x= {"No":np.arange(len(comments)),"Comments": comments,"Polarity":polarity}
+r = Rake()
+r.extract_keywords_from_text(extract_text_yt())
+for rating_y, keyword_yt in r.get_ranked_phrases_with_scores():
+    if rating_y > 10:
+        keywords_yt.append(keyword_yt)
+        keywords_yt.append(rating_y)
 
-df_yt_pol = pd.DataFrame(x)
+df_yt = pd.DataFrame(keywords_yt)
 
-#KPIs
-avg_polarity_yt = df_yt_pol['Polarity'].sum()/len(polarity)
+#Finding the Fuzzywuzzy rating for Hashtags
 
-left_column, middle_column, right_column = st.columns(3)
-with left_column:
-    st.subheader("AVERAGE POLARITY:")
-    st.subheader(avg_polarity_yt)
-with middle_column:
-    st.subheader("")
-    st.subheader("")
-with right_column:
-    st.subheader("")
-    st.subheader("")
+hashtags_yt = ['#advancelevel','#alkuppiya','#onlineclasses','#liveclasses','#videolessons','#studymotivations','#elearning','#exam','#free','#youtube']
+rating_yt = []
 
-yt_polarity_chart = df_yt_pol.groupby(by=["No"]).sum()[["Polarity"]]
-fig_yt_pol= px.bar(
-   yt_polarity_chart,
-    x=df_yt_pol["No"],
-    y="Polarity",
-    title="<b>Polarity of YouTube Comments</b>",
-    color_discrete_sequence=["#d62728"] * len(yt_polarity_chart),
+for words_yt in hashtags_yt:
+    ratio_yt = fuzz.WRatio(words_yt,df_yt[0])
+    rating_yt.append(ratio_yt)
+
+df_com_yt = pd.DataFrame({'Hashtags':hashtags_yt,
+                     'Fuzzywuzzy Rating':rating_yt })  
+
+# Visualization rating for top 10 YouTube Hashtags
+
+hashtags_chart_yt = df_com_yt.groupby(by=["Hashtags"]).sum()[["Fuzzywuzzy Rating"]]
+fig_keyword_yt= px.bar(
+    hashtags_chart_yt,
+    x=hashtags_chart_yt.index,
+    y="Fuzzywuzzy Rating",
+    title="<b>YouTube channel Hashtag Rating for top 10 Hashtags</b>",
+    color_discrete_sequence=["#d62728"] * len(hashtags_chart_yt),
     template="plotly_dark",
-    text_auto=True
+    text_auto=True,
+    #color="Fuzzywuzzy Rating"
 )
 
-st.plotly_chart(fig_yt_pol,use_container_width=True,)                  
+st.plotly_chart(fig_keyword_yt,use_container_width=True)
+
+#.................... Hashtag Analysis of Facebook............................
+
+st.image(img1) # Separator
+
+#text extraction from Facebook page
+
+def extract_text_fb():
+    s_fb = HTMLSession()
+    url_fb = 'https://www.facebook.com/Alkuppiya'
+    response_fb = s_fb.get(url_fb)
+    return response_fb.html.find(first=True).text
+
+# Separating keywords 
+
+keywords_fb= []
+rating_fb =[]
+
+r = Rake()
+r.extract_keywords_from_text(extract_text_fb())
+for rating_f, keyword_fb in r.get_ranked_phrases_with_scores():
+    if rating_f > 10:
+        keywords_fb.append(keyword_fb)
+        keywords_yt.append(rating_f)
+
+df_fb = pd.DataFrame(keywords_fb)
+
+#Finding the Fuzzywuzzy rating for Hashtags
+
+hashtags_fb = ['#advancelevel','#alkuppiya','#onlineclasses','#liveclasses','#videolessons','#studymotivations','#elearning','#exam','#free','#youtube']
+rating_fb = []
+
+for words_fb in hashtags_fb:
+    ratio_fb = fuzz.WRatio(words_fb,df_fb[0])
+    rating_fb.append(ratio_fb)
+
+df_com_fb = pd.DataFrame({'Hashtags':hashtags_fb,
+                     'Fuzzywuzzy Rating':rating_fb})  
 
 
-#....................Polarity Analysis of Facebook Comments..............................
-
-st.image(img1)
-
-df_fb_com = pd.read_json('https://raw.githubusercontent.com/NishanD21/SNA/main/Information%20Diffusion/fb_comments.json')
-
-#Polarity Analysis using NLP
-
-comments_fb = df_fb_com["Comment"]
-polarity_fb =[]
-
-for comment_fb in comments_fb:
-    review_fb1 = TextBlob(comment_fb)
-    rev_fb1 = review_fb1.sentiment.polarity
-    polarity_fb.append(rev_fb1)
-
-y= {"No":np.arange(len(comments_fb)),"Comments": comments_fb,"Polarity":polarity_fb}
-
-df_fb_pol = pd.DataFrame(y)
-
-#KPIs
-avg_polarity_fb = df_fb_pol['Polarity'].sum()/len(polarity_fb)
-
-left_column1, middle_column2, right_column3 = st.columns(3)
-with left_column1:
-    st.subheader("AVERAGE POLARITY:")
-    st.subheader(avg_polarity_fb)
-with middle_column2:
-    st.subheader("")
-    st.subheader("")
-with right_column3:
-    st.subheader("")
-    st.subheader("")
-
-fb_polarity_chart = df_fb_pol.groupby(by=["No"]).sum()[["Polarity"]]
-fig_fb_pol= px.bar(
-   fb_polarity_chart,
-    x=df_fb_pol["No"],
-    y="Polarity",
-    title="<b>Polarity of Facebook Comments</b>",
-    color_discrete_sequence=["#0083B8"] * len(yt_polarity_chart),
+hashtags_chart_fb = df_com_fb.groupby(by=["Hashtags"]).sum()[["Fuzzywuzzy Rating"]]
+fig_keyword_fb= px.bar(
+    hashtags_chart_fb,
+    x=hashtags_chart_fb.index,
+    y="Fuzzywuzzy Rating",
+    title="<b>Facebook page Hashtag Rating for top 10 Hashtags</b>",
+    color_discrete_sequence=["#0083B8"] * len(hashtags_chart_fb),
     template="plotly_dark",
-    text_auto=True
+    text_auto=True,
+    #color="Fuzzywuzzy Rating"
 )
 
-st.plotly_chart(fig_fb_pol,use_container_width=True,)   
+st.plotly_chart(fig_keyword_fb,use_container_width=True)
+
+#.................... Hashtag Analysis of Instagram............................
+
+st.image(img3) # Separator
+
+#text extraction from Instagram page
+
+def extract_text_in():
+    s_in = HTMLSession()
+    url_in = 'https://www.instagram.com/al_kuppiya/'
+    response_in = s_in.get(url_in)
+    return response_in.html.find(first=True).text
+
+# Separating keywords 
+
+keywords_in= []
+rating_in =[]
+
+r = Rake()
+r.extract_keywords_from_text(extract_text_in())
+for rating_i, keyword_in in r.get_ranked_phrases_with_scores():
+    if rating_i > 10:
+        keywords_in.append(keyword_in)
+        keywords_in.append(rating_i)
+
+df_in = pd.DataFrame(keywords_in)
+
+#Finding the Fuzzywuzzy rating for Hashtags
+
+hashtags_in = ['#advancelevel','#alkuppiya','#onlineclasses','#liveclasses','#videolessons','#studymotivations','#elearning','#exam','#free','#youtube']
+rating_in = []
+
+for words_in in hashtags_in:
+    ratio_in = fuzz.WRatio(words_in,df_in[0])
+    rating_in.append(ratio_in)
+
+df_com_in = pd.DataFrame({'Hashtags':hashtags_in,
+                     'Fuzzywuzzy Rating':rating_in})  
+
+
+hashtags_chart_in = df_com_in.groupby(by=["Hashtags"]).sum()[["Fuzzywuzzy Rating"]]
+fig_keyword_in= px.bar(
+    hashtags_chart_in,
+    x=hashtags_chart_in.index,
+    y="Fuzzywuzzy Rating",
+    title="<b>Instagram page Hashtag Rating for top 10 Hashtags</b>",
+    color_discrete_sequence=["#9467bd"] * len(hashtags_chart_in),
+    template="plotly_dark",
+    text_auto=True,
+    #color="Fuzzywuzzy Rating"
+)
+
+st.plotly_chart(fig_keyword_in,use_container_width=True)
+
 
 hide_stremlit_style = """
 <style>

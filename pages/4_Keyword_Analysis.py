5aed295098115d0bae8c11545320b3e9fc0e1747
Nishan Obeyesekera <108488940+NishanD21@users.noreply.github.com>
2022-08-20 00:12:44 +0530

    Update 4_Keyword_Analysis.py

--- pages/4_Keyword_Analysis.py
+++ pages/4_Keyword_Analysis.py
@@ -12,9 +12,9 @@ from io import BytesIO # to assist importing images to User Interface (UI)
 from textblob import TextBlob #For natural language processing
 import streamlit as st # for UI design
 
-#Importing Images to user interface (UI)
+#....................Polarity Analysis of YouTube Comments..............................
 
-url0 = "https://raw.githubusercontent.com/NishanD21/SNA/main/Images/Keyword%20Analysis.png"
+url0 = "https://raw.githubusercontent.com/NishanD21/SNA/main/Images/pOLARITY.png"
 url1 = "https://raw.githubusercontent.com/NishanD21/SNA/main/Images/FB1.png"
 url2 = "https://raw.githubusercontent.com/NishanD21/SNA/d4d282cba51f29e11e4cb91c61fe4431ef88f7fd/Images/YT1.png"
 url3 = "https://raw.githubusercontent.com/NishanD21/SNA/main/Images/Insta.png"
@@ -47,6 +47,98 @@ st.sidebar.markdown("<h2 style='text-align: center;'> </h2>", unsafe_allow_html=
 st.sidebar.markdown("<h2 style='text-align: center;'> </h2>", unsafe_allow_html=True)
 
 st.sidebar.image(img00)
+
+
+df_yt_com = pd.read_json('https://raw.githubusercontent.com/NishanD21/SNA/main/Information%20Diffusion/YT_Comments.json')
+
+#Polarity Analysis using NLP
+
+comments = df_yt_com["Comment"]
+polarity =[]
+
+for comment in comments:
+    review_1 = TextBlob(comment)
+    rev1 = review_1.sentiment.polarity
+    polarity.append(rev1)
+
+x= {"No":np.arange(len(comments)),"Comments": comments,"Polarity":polarity}
+
+df_yt_pol = pd.DataFrame(x)
+
+#KPIs
+avg_polarity_yt = df_yt_pol['Polarity'].sum()/len(polarity)
+
+left_column, middle_column, right_column = st.columns(3)
+with left_column:
+    st.subheader("AVERAGE POLARITY:")
+    st.subheader(avg_polarity_yt)
+with middle_column:
+    st.subheader("")
+    st.subheader("")
+with right_column:
+    st.subheader("")
+    st.subheader("")
+
+yt_polarity_chart = df_yt_pol.groupby(by=["No"]).sum()[["Polarity"]]
+fig_yt_pol= px.bar(
+   yt_polarity_chart,
+    x=df_yt_pol["No"],
+    y="Polarity",
+    title="<b>Polarity of YouTube Comments</b>",
+    color_discrete_sequence=["#d62728"] * len(yt_polarity_chart),
+    template="plotly_dark",
+    text_auto=True
+)
+
+st.plotly_chart(fig_yt_pol,use_container_width=True,)                  
+
+
+#....................Polarity Analysis of Facebook Comments..............................
+
+st.image(img1)
+
+df_fb_com = pd.read_json('https://raw.githubusercontent.com/NishanD21/SNA/main/Information%20Diffusion/fb_comments.json')
+
+#Polarity Analysis using NLP
+
+comments_fb = df_fb_com["Comment"]
+polarity_fb =[]
+
+for comment_fb in comments_fb:
+    review_fb1 = TextBlob(comment_fb)
+    rev_fb1 = review_fb1.sentiment.polarity
+    polarity_fb.append(rev_fb1)
+
+y= {"No":np.arange(len(comments_fb)),"Comments": comments_fb,"Polarity":polarity_fb}
+
+df_fb_pol = pd.DataFrame(y)
+
+#KPIs
+avg_polarity_fb = df_fb_pol['Polarity'].sum()/len(polarity_fb)
+
+left_column1, middle_column2, right_column3 = st.columns(3)
+with left_column1:
+    st.subheader("AVERAGE POLARITY:")
+    st.subheader(avg_polarity_fb)
+with middle_column2:
+    st.subheader("")
+    st.subheader("")
+with right_column3:
+    st.subheader("")
+    st.subheader("")
+
+fb_polarity_chart = df_fb_pol.groupby(by=["No"]).sum()[["Polarity"]]
+fig_fb_pol= px.bar(
+   fb_polarity_chart,
+    x=df_fb_pol["No"],
+    y="Polarity",
+    title="<b>Polarity of Facebook Comments</b>",
+    color_discrete_sequence=["#0083B8"] * len(yt_polarity_chart),
+    template="plotly_dark",
+    text_auto=True
+)
+
+st.plotly_chart(fig_fb_pol,use_container_width=True,)   
 
 hide_stremlit_style = """
 <style>
